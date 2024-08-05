b5a3a86f2577756c451be9782fc45f4551a1fd16
Dohoda <173878821+Dohoda@users.noreply.github.com>
2024-08-05 08:05:11 +0300

    added buttons to javascript

--- myscript.js
+++ myscript.js
@@ -18,3 +18,8 @@ function multiply(num1,num2){
 function divide(num1,num2){
     return num1 / num2;
 }
+
+const buttons = ["0","1","2","3",
+                 "4","5","6","7",
+                 "8","9","+","-",
+                 "*","/","clr","entr"];
